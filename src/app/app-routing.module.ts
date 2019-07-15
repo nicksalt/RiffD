3e1363d244234244665e2293730a44b3834f7906
Nick Salt <nicksalt24@gmail.com>
2019-07-15 14:47:40 -0400

    Finished view for pages

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,13 +5,11 @@ const routes: Routes = [
   { path: '', redirectTo: 'login-signup', pathMatch: 'full' },
   { path: 'home', loadChildren: './home/home.module#HomePageModule' },
   { path: 'login-signup', loadChildren: './login-signup/login-signup.module#LoginSignupPageModule' },
-<<<<<<< HEAD
   { path: 'signup', loadChildren: './signup/signup.module#SignupPageModule' },
-=======
-  { path: 'start-session', loadChildren: './practice/start-session/start-session.module#StartSessionPageModule' },
-  { path: 'practice-select-module', loadChildren: './practice-select-module/practice-select-module.module#PracticeSelectModulePageModule'},
-  { path: 'practice-module-practice', loadChildren: './practice-module-practice/practice-module-practice.module#PracticeModulePracticePageModule' }
->>>>>>> origin/master
+  {path: 'start-session', loadChildren: './practice/start-session/start-session.module#StartSessionPageModule'},
+  // tslint:disable-next-line: max-line-length
+  {path: 'practice-module-practice', loadChildren: './practice-module-practice/practice-module-practice.module#PracticeModulePracticePageModule'},
+  {path: 'practice-select-module', loadChildren: './practice-select-module/practice-select-module.module#PracticeSelectModulePageModule'}
 ];
 
 @NgModule({
