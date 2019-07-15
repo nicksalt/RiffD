5439927e84fb712364770bff8222ef026bfe66e8
Timothy Smith <tsontario@gmail.com>
2019-07-14 22:09:49 -0400

    initial page component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,7 +7,8 @@ const routes: Routes = [
   { path: 'login-signup', loadChildren: './login-signup/login-signup.module#LoginSignupPageModule' },
   { path: 'start-session', loadChildren: './practice/start-session/start-session.module#StartSessionPageModule' },
   { path: 'practice-select-module', loadChildren: './practice-select-module/practice-select-module.module#PracticeSelectModulePageModule'},
-  { path: 'practice-module-practice', loadChildren: './practice-module-practice/practice-module-practice.module#PracticeModulePracticePageModule' }
+  { path: 'practice-module-practice', loadChildren: './practice-module-practice/practice-module-practice.module#PracticeModulePracticePageModule' },
+  { path: 'practice-single-session-element', loadChildren: './practice-single-session-element/practice-single-session-element.module#PracticeSingleSessionElementPageModule' }
 ];
 
 @NgModule({
